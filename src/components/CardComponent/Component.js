e46bc1648e1073eb9115546113a37dbb46e85aac
Tarso Jabbes <tarsojabbes@gmail.com>
2023-05-06 23:01:04 -0300

    Atualiza README e inclui documentação do componente

--- src/components/CardComponent/Component.js
+++ src/components/CardComponent/Component.js
@@ -1,12 +1,23 @@
-import { useState } from "react";
 import styles from "./Component.module.css"
 
+/**
+ * Componente React que renderiza uma lista de cartões com o nome de cada item do JSON.
+ * Se a nota for menor que 50, o nome aparecerá em vermelho; caso contrário, aparecerá em verde.
+ * @param {object[]} data - Array contendo objetos com os campos "nome" e "nota".
+ * @returns Componente React que renderiza uma lista de cartões com o nome de cada item do JSON.
+ */
 export default function Component({data}) {
 
+    /**
+     * Verifica se a nota é maior ou igual a 50.
+     * @param {number} grade - Nota a ser verificada.
+     * @returns Verdadeiro se a nota for maior ou igual a 50, falso caso contrário.
+     */
     const hasPassingGrade = (grade) => {
         return grade >= 50;
     }
 
+    
     return (
         <>
         {data && data.map((item) => (
